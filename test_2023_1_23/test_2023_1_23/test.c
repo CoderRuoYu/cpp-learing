100972b84a9dda1ade0512560a94d4e7047dc7e6
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-01-23 10:21:07 +0800

    test_2023_1_23

--- test_2023_1_23/test_2023_1_23/test.c
+++ test_2023_1_23/test_2023_1_23/test.c
@@ -1,9 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+void search(int arr[], int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		int tem = arr[i];
+		for (int j = i + 1; j < sz; j++)
+		{
+			if (tem == arr[j])
+			{
+				printf("%d ", arr[j]);
+			}
+		}
+	}
+}
 int main()
 {
-	int arr[] = { 1,2,6,7,8,9,0,2,6 };
+	int arr[] = { 1,2,3,4,5,6,7,8,9,1,2 };
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	int* p = (int*)malloc(sz * sizeof(int));
+	search(arr, sz);
 	return 0;
 }

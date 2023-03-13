1044af4c5a67880d3badbbe58e2b1b198e8afb6f
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-03-13 21:00:34 +0800

    test_2023_3_13

---
+++ test3_12/test3_12/源.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+#pragma omp parallel sections
+	{
+#pragma omp section
+		for (int i = 0; i < 5; i++)
+		{
+			printf("section i: iteration %d by thread no. %d \n", i, omp_get_thread_num());
+		}
+#pragma omp section
+		for (int j = 0; j < 5; j++)
+		{
+			printf("section j: iteration %d by thread no. %d \n", j, omp_get_thread_num());
+
+		}
+	}
+	return 0;
+}
